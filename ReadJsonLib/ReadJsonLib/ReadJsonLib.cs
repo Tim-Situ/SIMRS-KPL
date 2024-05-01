757270bc4f45706d867f82312b9593cb941d1cf9
Muhammad Haulul Azkiyaa <haululazkiyaa@Muhammads-MacBook-Air.local>
2024-05-01 22:45:02 +0700

    menambahkan library ReadJsonLib

---
+++ ReadJsonLib/ReadJsonLib/ReadJsonLib.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+namespace ReadJsonLib;
+
+public class ReadJsonLib<T>
+{
+    public T ReadJsonFromFile(String path)
+    {
+        string filepath = path;
+        // membaca file json
+        string jsonString = File.ReadAllText(filepath);
+        // deserialize file json menjadi object
+        T data = JsonSerializer.Deserialize<T>(jsonString);
+        return data;
+    }
+}
+
+/*
+EXAMPLE TO USE:
+
+ReadJsonLib<Mahasiswa> mhs = new ReadJsonLib<Mahasiswa>();
+Mahasiswa mhsnew = mhs.ReadJson("../../../data.json");
+Console.WriteLine(mhsnew.nim);
+*/
