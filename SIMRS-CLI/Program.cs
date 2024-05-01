627372037264515c4e4940b828decb79b974d0c8
Muhammad Nur Shodiq <nursodik.2004@gmail.com>
2024-05-01 23:14:15 +0700

    Table-driven draft

--- SIMRS-CLI/Program.cs
+++ SIMRS-CLI/Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using SIMRS_CLI;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        Hari hariIni = Hari.Senin;
+        Console.WriteLine("Hari pemeriksaan: " + hariIni);
+        Poli poli = Poli.ParuParu;
+        Console.WriteLine("Poli yang dipilih: " + poli);
+        JenisObat jenisObat = JenisObat.Pil;
+        Console.WriteLine("Jenis obat: " +  jenisObat);
     }
 }
