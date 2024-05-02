020e1f7e4b51b7e502efe307d1c072e54bfc8396
Muhammad Nur Shodiq <nursodik.2004@gmail.com>
2024-05-02 17:51:07 +0700

    Menambah table-driven Jenis Kelamin

--- SIMRS-CLI/Program.cs
+++ SIMRS-CLI/Program.cs
@@ -5,11 +5,5 @@ internal class Program
     private static void Main(string[] args)
     {
 
-        Hari hariIni = Hari.Senin;
-        Console.WriteLine("Hari pemeriksaan: " + hariIni);
-        Poli poli = Poli.ParuParu;
-        Console.WriteLine("Poli yang dipilih: " + poli);
-        JenisObat jenisObat = JenisObat.Pil;
-        Console.WriteLine("Jenis obat: " +  jenisObat);
     }
 }
