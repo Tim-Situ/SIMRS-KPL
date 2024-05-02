fbc36cec53986e794ff3f44ce6f9b227551e64f7
IksanRisandy <iksanrisandy@gmail.com>
2024-05-02 14:48:59 +0700

    menambahkan state based/automata

--- SIMRS-CLI/Program.cs
+++ SIMRS-CLI/Program.cs
@@ -2,6 +2,14 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Tes Implementasi Automata
+        StatusPasien status_pasien = new StatusPasien();
+        Console.WriteLine("Status Pasien : " + status_pasien.currentState);
+        status_pasien.ActivateTrigger(Trigger.RESERVASI);
+        status_pasien.ActivateTrigger(Trigger.PERIKSA_PASIEN);
+        status_pasien.ActivateTrigger(Trigger.SELESAI_PEMERIKSAAN);
+        status_pasien.ActivateTrigger(Trigger.AMBIL_OBAT);
+        status_pasien.ActivateTrigger(Trigger.TRANSAKSI);
+        status_pasien.ActivateTrigger(Trigger.SELESAI_TRANSAKSI);
     }
 }
