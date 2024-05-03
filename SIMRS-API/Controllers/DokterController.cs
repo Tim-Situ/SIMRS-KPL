20752c568448c1739c197ff960af70b0e0cbd983
Fauzein Mulya Warman <fauzeinmulyawarman@Fauzeins-MacBook-Pro.local>
2024-05-03 22:09:33 +0700

    review dan penyesuaian dev-haulul

--- SIMRS-API/Controllers/DokterController.cs
+++ SIMRS-API/Controllers/DokterController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using ReadJsonLib;
 using SIMRS_LIB;
 
 namespace SIMRS_API;
--- SIMRS-API/Controllers/PoliController.cs
+++ SIMRS-API/Controllers/PoliController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
-using ReadJsonLib;
 using SIMRS_LIB;
 
 namespace SIMRS_API;
