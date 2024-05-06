d9deb0063f40eb907edddf26d2991d551830c5b5
diasdigital <78405123+diasdigital@users.noreply.github.com>
2024-05-06 10:36:31 +0700

    Menambah implementasi awal call API untuk post dan delete

--- SIMRS-CLI/ClientSideApi/ApiClient.cs
+++ SIMRS-CLI/ClientSideApi/ApiClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace SIMRS_CLI.ClientSideApi
 {
     public class ApiClient<T>
     {
+        // TO DO: Menambah try catch
+
         protected static HttpClient client = new HttpClient()
         {
             BaseAddress = new Uri("http://localhost:5117/api/")
@@ -24,6 +27,25 @@ namespace SIMRS_CLI.ClientSideApi
                 listData = await response.Content.ReadAsAsync<ApiResponse<T>>();
             }
             return listData.data;
+        }
+
+        public static async Task ClientPostData<U>(U data, string path)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                path, data);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public static async Task ClientPutData<V>(V data, string path)
+        {
+
+        }
+
+        public static async Task<HttpStatusCode> ClientDeleteData(string path)
+        {
+            HttpResponseMessage response = await client.DeleteAsync(
+                path);
+            return response.StatusCode;
         }
     }
 }
--- SIMRS-CLI/MenuAplikasi.cs
+++ SIMRS-CLI/MenuAplikasi.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Humanizer;
 using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.CodeAnalysis.Text;
 using SIMRS_API;
 using SIMRS_CLI.ClientSideApi;
 using SIMRS_CLI.Config;
@@ -24,9 +26,39 @@ namespace SIMRS_CLI
                 );
         }
 
-        public async Task<List<Pasien>> callGetApi()
+        public async Task<List<Pasien>> callGetApi(string path)
         {
-            return await ApiClient<List<Pasien>>.ClientGetData("Pasien");
+            return await ApiClient<List<Pasien>>.ClientGetData(path);
+        }
+        public async Task callPostApi(Pasien data, string path)
+        {
+            await ApiClient<Pasien>.ClientPostData(data, path);
+        }
+
+        public async Task<HttpStatusCode> callDeleteApi(string path)
+        {
+            return await ApiClient<Pasien>.ClientDeleteData(path);
+        }
+
+        public void TabelPasien()
+        {
+            List<String> kolom = new List<string>
+                {
+                    "Nama",
+                    "Tanggal Lahir",
+                    "No Hp",
+                    "Jenis Kelamin",
+                    "Alamat"
+                };
+
+            TableUtils tblPasien = new TableUtils(kolom);
+            List<Pasien> dataPasien = callGetApi("Pasien").Result;
+            foreach (Pasien pasien in dataPasien)
+            {
+                tblPasien.addData(new List<string> { pasien.nama, pasien.tglLahir, pasien.noHp, pasien.jnsKelamin.ToString(), pasien.alamat });
+            }
+
+            tblPasien.showData();
         }
 
         public void MenuUtama()
@@ -139,29 +171,14 @@ namespace SIMRS_CLI
         public void MenuPasien()
         {
             int pilihan = -1;
+            int idxPasien = -1;
             while (pilihan != 0)
             {
                 headerMenu();
 
                 Console.WriteLine("======== Halaman Pasien =========");
 
-                List<String> kolom = new List<string>
-                {
-                    "Nama",
-                    "Tanggal Lahir",
-                    "No Hp",
-                    "Jenis Kelamin",
-                    "Alamat"
-                };
-
-                TableUtils tblPasien = new TableUtils(kolom);
-                List<Pasien> dataPasien = callGetApi().Result;
-                foreach (Pasien pasien in dataPasien)
-                {
-                    tblPasien.addData(new List<string> { pasien.nama, pasien.tglLahir, pasien.noHp, pasien.jnsKelamin.ToString(), pasien.alamat });
-                }
-
-                tblPasien.showData();
+                TabelPasien();
 
                 Console.WriteLine(
                     "[1] Tambah Data Pasien" +
@@ -177,16 +194,17 @@ namespace SIMRS_CLI
                 switch (pilihan)
                 {
                     case 1:
-
+                        callPostApi(new Pasien("PSN003", "Nasi Goreng", "06-05-2024", "08", User.EnumJenisKelamin.PRIA, "Piring"), "Pasien").GetAwaiter().GetResult();
+                        
                         break;
                     case 2:
-                        Console.WriteLine("Tambah Data Pasien");
+                        Console.WriteLine("Pasien nomor berapa yang ingin diedit?");
                         break;
                     case 3:
-                        Console.WriteLine("Edit Data Pasien");
-                        break;
-                    case 4:
-                        Console.WriteLine("Hapus Data Pasien");
+                        Console.WriteLine("Pasien nomor berapa yang ingin dihapus?");
+                        idxPasien = Convert.ToInt32(Console.ReadLine())-1;
+                        var statusCode = callDeleteApi($"Pasien/{idxPasien}").GetAwaiter().GetResult();
+                        Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
                         break;
                 };
             }
