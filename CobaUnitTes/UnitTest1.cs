87c58dcb57dc6efdcafbcfd49bd7193c9ed55cd4
Fauzein Mulya Warman <fauzeinmw2@gmail.com>
2024-05-07 11:05:18 +0700

    add CobaUnitTesting : untuk hitung total biaya dan uang kembalian

--- CobaUnitTes/UnitTest1.cs
+++ CobaUnitTes/UnitTest1.cs
@@ -14,12 +14,32 @@ public class UnitTest1
         Pasien pasien = new Pasien("1242323", "Fauzein Mulya Warman", "02-12-2003", "08674557654", User.EnumJenisKelamin.PRIA, "Padang");
         Obat obat = new Obat("OBT01", "Paracetamol", 5000, Obat.EnumObat.PIL);
         Pemeriksaan pemeriksaan = new Pemeriksaan("PRS01", pasien, dokter, "07-05-2024", 160, 60, 90, "Pusing", "Sakit Kepala", obat);
-        Pembayaran pembayaran = new Pembayaran("BYR01", pemeriksaan, 60000);
+        Pembayaran pembayaran = new Pembayaran("BYR01", pemeriksaan);
 
         int expectTotalBiaya = 50000; // biaya Poli (45000) + biaya obat (5000)
 
         int hasilTotalBiaya = pembayaran.getTotalBiaya();
 
         Assert.AreEqual(expectTotalBiaya, hasilTotalBiaya);
+    }
+
+    [TestMethod]
+    public void TesUangKembalian()
+    {
+        Poli poli = new Poli("PLI02", "Gigi", "POLI0102", 100000);
+        Dokter dokter = new Dokter("1234222", "Dr. Ayu", poli, "01-01-2002", "08574868", User.EnumJenisKelamin.WANITA, "Jakarta");
+        Pasien pasien = new Pasien("1242323", "Rafatar", "02-12-2003", "08674557654", User.EnumJenisKelamin.PRIA, "Jakarta");
+        Obat obat = new Obat("OBT01", "Obat Sakit Gigi", 30000, Obat.EnumObat.PIL);
+        Pemeriksaan pemeriksaan = new Pemeriksaan("PRS01", pasien, dokter, "07-05-2024", 160, 60, 90, "Pusing", "Sakit Kepala", obat);
+        Pembayaran pembayaran = new Pembayaran("BYR01", pemeriksaan);
+
+        pembayaran.uangBayar = 150000;
+
+        //Contoh Expect salah
+        int expectUangKembalian = 2000; // uang (150000) - total biaya (130000)
+
+        int hasilUangKembalian = pembayaran.getUangKembalian(pembayaran.uangBayar);
+
+        Assert.AreEqual(expectUangKembalian, hasilUangKembalian);
     }
 }
