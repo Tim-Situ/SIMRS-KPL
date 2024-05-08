74584e65496ab59ea5e3ae2179bf3541bf5e568b
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2024-05-08 08:44:40 +0700

    menambahkan debugging

---
+++ SIMRS-CLI/Debugging.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using SIMRS_CLI.Config;
+using SIMRS_CLI.Models;
+
+namespace SIMRS_CLI
+{
+    public class Debugging
+    {
+        public static void DebugMenu()
+        {
+            MenuLanguage menu = LanguageConfig.getMenu;
+
+            if (menu.main_menu.Count != 6)
+            {
+                Console.WriteLine("KESALAHAN: Jumlah main menu tidak sesuai -> " + menu.main_menu.Count);
+            }
+            else if (menu.patient_menu.Count != 3)
+            {
+                Console.WriteLine("KESALAHAN: Jumlah main menu tidak sesuai -> " + menu.main_menu.Count);
+            }
+            else if (menu.patient_add.Count != 6)
+            {
+                Console.WriteLine("KESALAHAN: Jumlah main menu tidak sesuai -> " + menu.main_menu.Count);
+            }
+            else
+            {
+                Console.WriteLine("Berhenti");
+            }
+            Console.WriteLine("Selesai");
+        }
+    }
+}
+
