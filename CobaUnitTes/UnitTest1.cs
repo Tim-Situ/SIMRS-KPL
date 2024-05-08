8acbe651d599fbdfe5dc8ddcc3bfa8da19683586
diasdigital <78405123+diasdigital@users.noreply.github.com>
2024-05-08 13:32:54 +0700

    Unit test menambah TesGetDataPoli

--- CobaUnitTes/UnitTest1.cs
+++ CobaUnitTes/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using SIMRS_API;
+using SIMRS_CLI.ClientSideApi;
 using SIMRS_CLI.Config;
 
 namespace CobaUnitTes;
@@ -13,7 +14,7 @@ public class UnitTest1
         Dokter dokter = new Dokter("1234222", "Dr. Anwar", poli, "01-01-2002", "08574868", User.EnumJenisKelamin.PRIA, "Jakarta");
         Pasien pasien = new Pasien("1242323", "Fauzein Mulya Warman", "02-12-2003", "08674557654", User.EnumJenisKelamin.PRIA, "Padang");
         Obat obat = new Obat("OBT01", "Paracetamol", 5000, Obat.EnumObat.PIL);
-        Pemeriksaan pemeriksaan = new Pemeriksaan("PRS01", pasien, dokter, "07-05-2024", 160, 60, 90, "Pusing", "Sakit Kepala", obat);
+        Pemeriksaan pemeriksaan = new Pemeriksaan("PRS01", pasien, dokter, "07-05-2024", 160, 60, "90", "Pusing", "Sakit Kepala", obat);
         Pembayaran pembayaran = new Pembayaran("BYR01", pemeriksaan);
 
         int expectTotalBiaya = 50000; // biaya Poli (45000) + biaya obat (5000)
@@ -30,7 +31,7 @@ public class UnitTest1
         Dokter dokter = new Dokter("1234222", "Dr. Ayu", poli, "01-01-2002", "08574868", User.EnumJenisKelamin.WANITA, "Jakarta");
         Pasien pasien = new Pasien("1242323", "Rafatar", "02-12-2003", "08674557654", User.EnumJenisKelamin.PRIA, "Jakarta");
         Obat obat = new Obat("OBT01", "Obat Sakit Gigi", 30000, Obat.EnumObat.PIL);
-        Pemeriksaan pemeriksaan = new Pemeriksaan("PRS01", pasien, dokter, "07-05-2024", 160, 60, 90, "Pusing", "Sakit Kepala", obat);
+        Pemeriksaan pemeriksaan = new Pemeriksaan("PRS01", pasien, dokter, "07-05-2024", 160, 60, "90", "Pusing", "Sakit Kepala", obat);
         Pembayaran pembayaran = new Pembayaran("BYR01", pemeriksaan);
 
         pembayaran.uangBayar = 150000;
@@ -57,5 +58,18 @@ public class UnitTest1
 
         // Assert
         Assert.AreEqual(LanguageConfig.defaultLang.lang, expected);
+    }
+
+    [TestMethod]
+    public void TesGetDataPoli()
+    {
+        // Arrange
+        ApiClient<Poli> api = new();
+
+        // Act
+        ApiResponse<List<Poli>> response = api.ClientGetData("Poli").GetAwaiter().GetResult();
+
+        // Assert
+        Assert.IsTrue(response.success);
     }
 }
