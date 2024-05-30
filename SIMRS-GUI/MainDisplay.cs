e75f7b64b905e0cc7e3f2ea2d15b60462a13b79e
Muhammad Nur Shodiq <nursodik.2004@gmail.com>
2024-05-31 01:27:25 +0700

    Penambahan icon

--- SIMRS-GUI/MainDisplay.cs
+++ SIMRS-GUI/MainDisplay.cs
@@ -6,5 +6,53 @@ namespace SIMRS_GUI
         {
             InitializeComponent();
         }
+
+        private void MainDisplayDraft_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nightControlBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PanelTop_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        bool sidebarExpand = true;
+        private void sidebarTransition_Tick(object sender, EventArgs e)
+        {
+            if (sidebarExpand)
+            {
+                sidebar.Width -= 10;
+                if (sidebar.Width <= 61)
+                {
+                    sidebarExpand = false;
+                    sidebarTransition.Stop();
+                }
+            }
+            else
+            {
+                sidebar.Width += 10;
+                if (sidebar.Width >= 222)
+                {
+                    sidebarExpand = true;
+                    sidebarTransition.Stop();
+                }
+            }
+        }
+
+        private void hamburgerMenu_Click(object sender, EventArgs e)
+        {
+            sidebarTransition.Start();
+        }
     }
 }
