92461d59b0a04711d450f8b4df7c496ccc03259f
Fauzein Mulya Warman <fauzeinmw2@gmail.com>
2024-06-01 19:20:40 +0700

    Menambahkan Otentikasi di API

---
+++ SIMRS-API/Controllers/AuthController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SIMRS_API.helpers;
+using SIMRS_LIB;
+
+namespace SIMRS_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private static string _jsonFilePath = "Data/DataAdmin.json";
+        private static List<Admin> _dataAdmin;
+        ApiResponse<Object> response = new ApiResponse<object>();
+
+        [HttpPost("register")]
+        public ActionResult<ApiResponse<Admin>> Register([FromBody] Admin value)
+        {
+            _dataAdmin = JsonUtils<List<Admin>>.ReadJsonFromFile(_jsonFilePath);
+            Admin cariAdmin = _dataAdmin.FirstOrDefault(item => item.username == value.username);
+
+            if (cariAdmin != null)
+            {
+                response.success = false;
+                response.message = "Username sudah digunakan";
+
+                return BadRequest(response);
+            }
+
+            value.password = PasswordHelper.HashPassword(value.password);
+
+            _dataAdmin.Add(value);
+            JsonUtils<List<Admin>>.WriteJsonFile(_dataAdmin, _jsonFilePath);
+
+            response.message = "Register Berhasil...";
+
+            return Ok(response);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<ApiResponse<Object>> Login([FromBody] Admin value)
+        {
+
+            _dataAdmin = JsonUtils<List<Admin>>.ReadJsonFromFile(_jsonFilePath);
+            Admin cariAdmin = _dataAdmin.FirstOrDefault(item => item.username == value.username);
+
+
+            if (cariAdmin == null || !PasswordHelper.VerifyPassword(value.password, cariAdmin.password))
+            {
+                response.success = false;
+                response.message = "Username atau Password Salah...";
+                return Unauthorized(response);
+            }
+
+            var token = JwtTokenHelper.GenerateToken(value.username);
+
+            response.message = "Login Berhasil...";
+            response.data = new { token = token };
+
+            return Ok(response);
+        }
+    }
+}
+
--- SIMRS-API/Program.cs
+++ SIMRS-API/Program.cs
@@ -1,21 +1,71 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Configure JWT authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "SIMRS-API", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please insert JWT with Bearer into field",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+    {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        },
+        new string[] { }
+    }});
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
